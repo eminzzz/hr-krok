82bcd850a808fb66886c9df0772bdffd58132e94
eminzzz <eminroth@gmail.com>
2021-12-02 14:52:12 +0200

    feat: implemented settings page

---
+++ diploma_front/src/views/Settings.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MainLayout } from "../layouts/MainLayout";
+
+export const Settings = () => {
+  return (
+    <MainLayout>
+      <h3>Will be implemented in future</h3>
+    </MainLayout>
+  );
+};
