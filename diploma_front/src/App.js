55454e238684e0a1ea92bc64fc407541ec9da8c0
eminzzz <eminroth@gmail.com>
2021-12-02 14:51:14 +0200

    feat: added 2 routes

--- diploma_front/src/App.js
+++ diploma_front/src/App.js
@@ -4,6 +4,8 @@ import { Routes, Route } from "react-router-dom";
 import { Home } from "./views/Home";
 import { About } from "./views/About";
 import { NotFound } from "./views/NotFound";
+import { Admins } from "./views/Admins";
+import { Settings } from "./views/Settings";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Route path="*" element={<NotFound />} />
       <Route path="/" exact element={<Home />} />
       <Route path="/about" exact element={<About />} />
+      <Route path="/admin/admins" exact element={<Admins />} />
+      <Route path="/admin/settings" exact element={<Settings />} />
     </Routes>
   );
 }
