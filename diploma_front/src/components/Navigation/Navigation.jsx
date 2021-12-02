08aaec35e4a8d382d440cc1c904d81518c29294d
eminzzz <eminroth@gmail.com>
2021-12-02 16:41:16 +0200

    feat: implemented Navigation

--- diploma_front/src/components/Navigation/Navigation.jsx
+++ diploma_front/src/components/Navigation/Navigation.jsx
@@ -1,8 +1,46 @@
 import React from "react";
+import styles from "./Navigation.module.scss";
+import { NavLink } from "react-router-dom";
 
-// TODO: implement Navigation
+import gearIcon from "./images/gear.png";
+import peaceIcon from "./images/peace.png";
+import planeIcon from "./images/plane.png";
+import robotIcon from "./images/robot.png";
+
 const Navigation = () => {
-  return <div>123</div>;
+  return (
+    <div className={styles.Navigation}>
+      <NavLink
+        to={"/"}
+        exact
+        activeClassName={styles.LinkActive}
+        className={styles.Link}
+      >
+        <img className={styles.Icon} src={planeIcon} alt="nav-icon" /> Vacancies
+      </NavLink>
+      <NavLink
+        to={"/admin/users"}
+        activeClassName={styles.LinkActive}
+        className={styles.Link}
+      >
+        <img className={styles.Icon} src={peaceIcon} alt="nav-icon" /> Users
+      </NavLink>
+      <NavLink
+        to={"/admin/admins"}
+        activeClassName={styles.LinkActive}
+        className={styles.Link}
+      >
+        <img className={styles.Icon} src={robotIcon} alt="nav-icon" /> Admins
+      </NavLink>
+      <NavLink
+        to={"/admin/settings"}
+        activeClassName={styles.LinkActive}
+        className={styles.Link}
+      >
+        <img className={styles.Icon} src={gearIcon} alt="nav-icon" /> Settings
+      </NavLink>
+    </div>
+  );
 };
 
 export default Navigation;
