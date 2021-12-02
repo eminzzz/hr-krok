69f73e99536e138252c7ecfb7c1f80e04b4095dc
eminzzz <eminroth@gmail.com>
2021-12-02 14:51:34 +0200

    feat: implemented Admins page

---
+++ diploma_front/src/views/Admins.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { MainLayout } from "../layouts/MainLayout";
+
+export const Admins = () => {
+  return (
+    <MainLayout>
+      <h3>Will be implemented in future</h3>
+    </MainLayout>
+  );
+};
